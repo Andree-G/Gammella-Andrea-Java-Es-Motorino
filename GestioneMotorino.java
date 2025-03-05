fd9e2b375462b2054544949229ebd5b78fe34ac6
Andree-G <gammella.andrea@itisgrassi.edu.it>
2025-03-05 09:45:43 +0100

    Esercizio 01

---
+++ GestioneMotorino.java
@@ -0,0 +1,19 @@
+
+
+public class GestioneMotorino
+{
+   public static void main(String[] args)
+   {
+     Motorino motorino = new Motorino ("Nero",10, "Ducati Panigale",false);
+     {
+        motorino.mostraDettagli();
+        motorino.setVelocità(10);  
+        System.out.println("Nuova velocità del motorino: " + motorino.getVelocità());
+        motorino.accellera(20);
+        motorino.mostraDettagli();
+        motorino.acceleraSeAntifurtoNonAttivo(5);
+        motorino.mostraDettagli();
+        motorino.InserisciAntifurto();
+     }
+   }
+}
---
+++ Motorino.java
@@ -0,0 +1,93 @@
+
+public class Motorino
+{
+   private String colore;
+   private double velocità;
+   private String tipo;
+   private boolean antifurto;
+
+    public Motorino(String colore, double velocità, String tipo, boolean antifurto)
+   {
+       this.colore = colore;
+       this.velocità = velocità;
+       this.tipo = tipo;
+       this.antifurto = antifurto;
+   }
+
+   public String getColore()
+   {
+       return colore;
+   }
+   public void setColore(String colore)
+   {
+       this.colore = colore;
+   }
+
+   public double getVelocità()
+   {
+       return velocità;
+   }
+   public void setVelocità(double velocità)
+   {
+        velocità += 50;
+        this.velocità = velocità;
+   }
+
+   public String getTipo()
+   {
+       return tipo;
+   }
+   public void setTipo(String tipo)
+   {
+       this.tipo = tipo;
+   }
+
+   public boolean getAntifurto()
+   {
+       return antifurto;
+   }
+   public void getAntifurto(boolean antifurto)
+   {
+       this.antifurto = antifurto;
+   }
+
+   public void accellera(double incremento)
+   {
+       velocità += incremento;
+   }
+
+   public void acceleraSeAntifurtoNonAttivo(double incremento)
+   {
+     if (!antifurto)
+     {
+        velocità += incremento;
+        System.out.println("Velocità aumentata di " + incremento + " Velocità attuale: " + velocità);
+     }
+     else
+     {
+        System.out.println("Nessun incremento");
+     }
+
+   }
+
+   public void InserisciAntifurto()
+   {
+        if (!antifurto)
+        {
+            antifurto = true;
+            System.out.println("Antifurto attivato");
+        }
+        else
+        {
+            System.out.println("L'antifurto è gia presente");
+        }
+   }
+
+
+
+   public void mostraDettagli()
+   {
+       System.out.println("Colore: " + colore + " velocità: " + velocità + " Tipo: " + tipo + " Antifurto " + antifurto);
+}
+}
+
